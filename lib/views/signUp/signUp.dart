bde720b83edb30bab096760df038db225ebedb3d
Michuxx <104158118+Michuxx@users.noreply.github.com>
2025-10-13 21:13:38 +0200

    Add input validation to sign up form
    Moved controllers into the _SignUpState class and added email and password validation with error messages. Updated input fields to display validation errors and improved the register logic to check inputs before proceeding.

--- lib/views/signUp/signUp.dart
+++ lib/views/signUp/signUp.dart
@@ -13,17 +13,71 @@ class SignUp extends StatefulWidget {
   State<SignUp> createState() => _SignUpState();
 }
 
-final emailController = TextEditingController();
-final passwordController = TextEditingController();
-final confirmPasswordController = TextEditingController();
-
-void register() {}
-
-void goToLogin(BuildContext context) {
-  Navigator.pop(context);
-}
-
 class _SignUpState extends State<SignUp> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  String? emailError;
+  String? passwordError;
+  String? confirmPasswordError;
+
+
+  final emailPattern = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+
+  bool emailValidation(email) {
+    final regex =RegExp(emailPattern);
+    if (email.isEmpty || !regex.hasMatch(email)) {
+      setState(() {
+        emailError = "Nieprawidłowy adres e-mail";
+      });
+      return false;
+    } else {
+      setState(() {
+        emailError = null;
+      });
+      return true;
+    }
+  }
+
+  bool passwordValidation(password, confirmPassword) {
+    if (password.length < 6) {
+      setState(() {
+        confirmPasswordError = "Hasło ma mniej niż 6 znaków";
+      });
+      return false;
+    } else if (password !=confirmPassword) {
+      setState(() {
+        confirmPasswordError = "Nie są takie same";
+      });
+      return false;
+    }
+    else {
+      setState(() {
+        confirmPasswordError = null;
+      });
+      return true;
+    }
+  }
+
+  void register() {
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
+    final confirmPassword = confirmPasswordController.text.trim();
+
+    final isEmailOk = emailValidation(email);
+    final isPassOk  = passwordValidation(password, confirmPassword);
+
+    if(isEmailOk && isPassOk) {
+      print("zarejestrowano");
+    }
+  }
+
+  void goToLogin(BuildContext context) {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,18 +96,21 @@ class _SignUpState extends State<SignUp> {
               Input(
                 placeholder: "Email",
                 controller: emailController,
+                errorText: emailError,
               ),
               SizedBox(height: 25),
               Input(
                 placeholder: "Password",
                 controller: passwordController,
                 obscureText: true,
+                errorText: passwordError,
               ),
               SizedBox(height: 25),
               Input(
                 placeholder: "Confirm Password",
                 controller: confirmPasswordController,
                 obscureText: true,
+                errorText: confirmPasswordError,
               ),
               SizedBox(height: 25),
               Authbutton(onPressed: register, label: 'Utwórz konto'),
